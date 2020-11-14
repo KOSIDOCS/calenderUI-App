a57105db57a1e7b2fb2cb69391a9cb57f390e688
kosidocs <kelvinonaru@gmail.com>
2020-11-14 06:12:04 +0400

    Updates

---
+++ precache-manifest.038d38a3eac2c44d825399d7b8538abb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596e3059ff30da19a8db85dd2327a257",
+    "url": "/calenderUI-App/index.html"
+  },
+  {
+    "revision": "64af866197796d5f0316",
+    "url": "/calenderUI-App/static/js/2.4fe411ac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/calenderUI-App/static/js/2.4fe411ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c3fe69be8322216a264",
+    "url": "/calenderUI-App/static/js/main.0c5362c1.chunk.js"
+  },
+  {
+    "revision": "57b2edc7d5f0ea08e4ba",
+    "url": "/calenderUI-App/static/js/runtime-main.abe1b3d9.js"
+  },
+  {
+    "revision": "50f525210b4ec4351bdfd437f1a8ad24",
+    "url": "/calenderUI-App/static/media/Woman.50f52521.jpeg"
+  },
+  {
+    "revision": "45d9bc1002161c5b13f2bd9c978fe81c",
+    "url": "/calenderUI-App/static/media/bikerace.45d9bc10.jpeg"
+  },
+  {
+    "revision": "1afd6993bebfc735418dbad4b1ca4565",
+    "url": "/calenderUI-App/static/media/car-on-stadium.1afd6993.jpeg"
+  },
+  {
+    "revision": "ac6bc967f0df569017bd15c1a76e9f21",
+    "url": "/calenderUI-App/static/media/cartrack.ac6bc967.jpeg"
+  },
+  {
+    "revision": "266b8a000aeaefe74c54b9fcf4ab5953",
+    "url": "/calenderUI-App/static/media/yellow-honda.266b8a00.jpeg"
+  }
+]);
